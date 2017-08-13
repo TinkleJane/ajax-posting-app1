db8cb917344e2f89a13d864cc2d7b928024368b6
xjane <tinklexjane@gmail.com>
2017-08-13 08:07:38 +0800

    ajax no end

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,16 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, :only => [:create, :destroy]
 
   def index
-    @posts = Post.order("id DESC").all
+    @posts = Post.order("id DESC").limit(10)
+
+    if params[:max_id]
+      @posts = @posts.where("id <?", params[:max_id]).limit(1)
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def create
